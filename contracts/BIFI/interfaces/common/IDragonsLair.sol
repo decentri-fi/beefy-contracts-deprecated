2617f1579cb976ef92b1270614d37abff89e44cb
QuintenDes <quintendeswaef@gmail.com>
2021-09-28 12:14:57 +0200

    Extraction

---
+++ contracts/BIFI/interfaces/common/IDragonsLair.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.0;
+
+interface IDragonsLair {
+    function enter(uint256 amount) external;
+    function leave(uint256 amount) external;
+    function dQUICKForQUICK(uint256 amount) external view returns (uint256);
+    function QUICKForDQUICK(uint256 amount) external view returns (uint256);
+}
---
+++ contracts/BIFI/interfaces/common/IRewardPool.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+interface IRewardPool {
+    function deposit(uint256 amount) external;
+    function stake(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function getReward() external;
+    function balanceOf(address account) external view returns (uint256);
+    function earned(address account) external view returns (uint256);
+}
